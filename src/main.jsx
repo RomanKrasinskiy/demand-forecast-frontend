7ef53e24d96f9c1e16b07729fcdb9caee386ac48
Konstantin Nivnikov <nivnikoff@gmail.com>
2023-09-27 14:28:21 +0400

    Conflicts resolved

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './components/App/App';
 import './index.css';
 import { store } from './components/store/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
