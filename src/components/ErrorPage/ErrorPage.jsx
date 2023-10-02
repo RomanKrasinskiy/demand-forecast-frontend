857d1b8d5be64da4ac605add702dd895b1bc8f34
Konstantin Nivnikov <nivnikoff@gmail.com>
2023-10-02 22:36:49 +0400

    ErrorPAge refactored

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -8,32 +8,34 @@ import MainBackground from "../Main/MainBackground/MainBackground";
 function ErrorPage() {
 
   return (
-    <>
+    <div className={ErrorPageCSS.page}>
+      <MainBackground/>
       <div className={ErrorPageCSS.container}>
         <Logo />
         <div className={ErrorPageCSS.textContainer}>
-          <h1 className={ErrorPageCSS.title}>Ошабка 404</h1>
+          <h1 className={ErrorPageCSS.title}>Ошибка 404</h1>
           <p className={ErrorPageCSS.text}>Мы не смогли найти информацию по вашему запросу</p>
-          <p className={ErrorPageCSS.linkText}>Вы можете <Link className={ErrorPageCSS.link}>вернуться на предыдущую страницу</Link></p>
+          <p className={ErrorPageCSS.linkText}>Вы можете <Link className={ErrorPageCSS.link} to='/'>вернуться на предыдущую страницу</Link></p>
         </div>
-        <Button 
-          variant="contained"
-          sx={{
-            width: '304px',
-            height: '60px',
-            backgroundColor: 'rgba(0, 60, 150, 1)',
-            borderRadius: '46px',
-            border: '2px solid rgba(0, 60, 150, 1)',
-            color: 'white',
-            fontSize: '20px',
-            fontFamily: 'Helvetica Neue',
-            textTransform: 'none',
-          }}
-          href="/"
-        >На главную</Button>
+        <Link to='/'>
+          <Button 
+            variant='contained'
+            sx={{
+              width: '304px',
+              height: '60px',
+              backgroundColor: 'rgba(0, 60, 150, 1)',
+              borderRadius: '46px',
+              border: '2px solid rgba(0, 60, 150, 1)',
+              color: 'white',
+              fontSize: '20px',
+              fontFamily: 'Helvetica Neue',
+              textTransform: 'none',
+            }}
+          >На главную</Button>
+        </Link>
       </div>
-      <MainBackground/>
-    </>
+      
+    </div>
   )
 }
 
