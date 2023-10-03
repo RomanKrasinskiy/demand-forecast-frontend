02bd0cf67b593e12c7fd2a79475c1d07e4aa0e3f
Konstantin Nivnikov <nivnikoff@gmail.com>
2023-10-03 19:05:22 +0400

    Graphs layout added

--- src/utils/ForecastData.js
+++ src/utils/ForecastData.js
@@ -1,5 +1,5 @@
 // Хардкод даты, вместо того, что забирается с API
-export const rows = [
+export const tableRows = [
   { id: 1, c1: 'Булочка', c2: '1', c3: '2', c4: '3', c5: '4', c6: '5', c7: '6', c8: '7', c9: '8', c10: '9', c11: '10', c12: '9', c13: '8', c14: '7', c15: '6'},
   { id: 2, c1: 'Булочка с маком', c2: '2', c3: '4', c4: '6', c5: '8', c6: '6', c7: '4', c8: '6', c9: '8', c10: '4', c11: '2', c12: '10', c13: '6', c14: '4', c15: '8'},
   { id: 3, c1: 'Булочка с повидлом', c2: '3', c3: '6', c4: '4', c5: '7', c6: '5', c7: '8', c8: '9', c9: '3', c10: '5', c11: '14', c12: '12', c13: '10', c14: '8', c15: '7'},
@@ -17,20 +17,95 @@ export const rows = [
   { id: 15, c1: 'Брокколи', c2: '0', c3: '0', c4: '1', c5: '0', c6: '2', c7: '0', c8: '1', c9: '0', c10: '1', c11: '0', c12: '0', c13: '2', c14: '0', c15: '1'},
 ];
 
-export const columns = [
-  { field: 'c1', headerName: 'Товар', width: 150, headerClassName: 'header'},
-  { field: 'c2', headerName: '2023-10-01', width: 90, headerClassName: 'header' },
-  { field: 'c3', headerName: '2023-10-02', width: 90, headerClassName: 'header' },
-  { field: 'c4', headerName: '2023-10-03', width: 90, headerClassName: 'header' },
-  { field: 'c5', headerName: '2023-10-04', width: 90, headerClassName: 'header' },
-  { field: 'c6', headerName: '2023-10-05', width: 90, headerClassName: 'header' },
-  { field: 'c7', headerName: '2023-10-06', width: 90, headerClassName: 'header' },
-  { field: 'c8', headerName: '2023-10-07', width: 90, headerClassName: 'header' },
-  { field: 'c9', headerName: '2023-10-08', width: 90, headerClassName: 'header' },
-  { field: 'c10', headerName: '2023-10-09', width: 90, headerClassName: 'header' },
-  { field: 'c11', headerName: '2023-10-10', width: 90, headerClassName: 'header' },
-  { field: 'c12', headerName: '2023-10-11', width: 90, headerClassName: 'header' },
-  { field: 'c13', headerName: '2023-10-12', width: 90, headerClassName: 'header' },
-  { field: 'c14', headerName: '2023-10-13', width: 90, headerClassName: 'header' },
-  { field: 'c15', headerName: '2023-10-14', width: 90, headerClassName: 'header' },
+export const tableColumns = [
+  { field: 'c1', headerName: 'Товар', width: 200, headerClassName: 'header'},
+  { field: 'c2', headerName: '2023-10-01', width: 145, headerClassName: 'header' },
+  { field: 'c3', headerName: '2023-10-02', width: 145, headerClassName: 'header' },
+  { field: 'c4', headerName: '2023-10-03', width: 145, headerClassName: 'header' },
+  { field: 'c5', headerName: '2023-10-04', width: 145, headerClassName: 'header' },
+  { field: 'c6', headerName: '2023-10-05', width: 145, headerClassName: 'header' },
+  { field: 'c7', headerName: '2023-10-06', width: 145, headerClassName: 'header' },
+  { field: 'c8', headerName: '2023-10-07', width: 145, headerClassName: 'header' },
+  { field: 'c9', headerName: '2023-10-08', width: 145, headerClassName: 'header' },
+  { field: 'c10', headerName: '2023-10-09', width: 145, headerClassName: 'header' },
+  { field: 'c11', headerName: '2023-10-10', width: 145, headerClassName: 'header' },
+  { field: 'c12', headerName: '2023-10-11', width: 145, headerClassName: 'header' },
+  { field: 'c13', headerName: '2023-10-12', width: 145, headerClassName: 'header' },
+  { field: 'c14', headerName: '2023-10-13', width: 145, headerClassName: 'header' },
+  { field: 'c15', headerName: '2023-10-14', width: 145, headerClassName: 'header' },
 ];
+
+export const chartRows = [
+  { id: 1, c1: 'Булочка'},
+  { id: 2, c1: 'Булочка с маком'},
+  { id: 3, c1: 'Булочка с повидлом'},
+  { id: 4, c1: 'Булочка Московская'},
+  { id: 5, c1: 'Булочка с изюмом'},
+  { id: 6, c1: 'Булочка Свердловская'},
+  { id: 7, c1: 'Булочка со сгущенкой'},
+  { id: 8, c1: 'Булочка с творогом'},
+  { id: 9, c1: 'Слойка с ветчиной'},
+  { id: 10, c1: 'Слойка с сыром'},
+  { id: 11, c1: 'Слойка с джемом'},
+  { id: 12, c1: 'Хачапури'},
+  { id: 13, c1: 'Беляш'},
+  { id: 14, c1: 'Чебурек'},
+  { id: 15, c1: 'Брокколи'},
+];
+
+export const chartColumns = [
+  { field: 'c1', headerName: 'Товар', width: '100%', headerClassName: 'header'},
+];
+
+export const data = {
+  labels: ['2023-10-01', '2023-10-02', '2023-10-03', '2023-10-04', '2023-10-05', '2023-10-06', '2023-10-07', '2023-10-08', '2023-10-09', '2023-10-10', '2023-10-11', '2023-10-12', '2023-10-13', '2023-10-14'],
+  datasets: [
+    {
+      label: 'Булочка с маком',
+      data: [ 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7 ],
+      backgroundColor: 'black',
+      borderColor: 'black',
+      pointBorderColor: '#003C96',
+    },
+    {
+      label: 'Чебурек',
+      data: [ 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3, 8, 3 ],
+      backgroundColor: '#FFB900',
+      borderColor: '#FFB900',
+      pointBorderColor: '#003C96',
+    },
+    {
+      label: 'Слойка с ветчиной',
+      data: [ 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6 ],
+      backgroundColor: '#D17CCB',
+      borderColor: '#D17CCB',
+      pointBorderColor: '#003C96',
+    },
+    {
+      label: 'Бородинский хлеб',
+      data: [ 9, 3, 9, 9, 3, 7, 5, 1, 0, 5, 8, 2, 0, 9 ],
+      backgroundColor: 'brown',
+      borderColor: 'brown',
+      pointBorderColor: '#003C96',
+    },
+    {
+      label: 'Броколли',
+      data: [ 7, 4, 9, 4, 4, 5, 9, 2, 3, 0, 7, 8, 1, 6 ],
+      backgroundColor: '#007841',
+      borderColor: '#007841',
+      pointBorderColor: '#003C96',
+    }
+  ]
+}
+
+export const options = {
+  plugins: {
+    legend: true
+  },
+  scales: {
+    y: {
+      min: 0,
+      max: 20
+    }
+  }
+}
